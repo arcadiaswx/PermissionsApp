56ceca609d8ca6d63773baa2fd878490bd189dcc
Craig Booker <info@itech.plus>
2018-06-03 11:49:21 -0500

    Added if/else statement within checkSelfPermission to catch when a user has already denied permission once and we need to explain why we need permission.

--- app/src/main/java/com/arcadiasoftworks/permissionsapp/MainActivity.java
+++ app/src/main/java/com/arcadiasoftworks/permissionsapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.arcadiasoftworks.permissionsapp;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,31 @@ public class MainActivity extends AppCompatActivity {
         callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if(checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)){
+                            // We need to explain to our users why we need this permission.
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("Call Permission")
+                                    .setMessage("Hi there! We can't call anyone without the call permission, could you please grant it?")
+                                    .setPositiveButton("Yep", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSIONS_REQUEST_CODE);
+                                        }
+                                    })
+                                    .setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            Toast.makeText(MainActivity.this, ":)", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).show();
+                        } else{
+                        // We don't have the permission and to make our call. We need to request permission to make the call.
+                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PERMISSIONS_REQUEST_CODE);
+                        }
                     } else {
+                        // We have the permission and we can make our call.
                         makeCall();
                     }
                 } else {
@@ -33,9 +61,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makeCall();
+            }
+        }
+    }
+
     private void makeCall() {
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + "12345678"));
+
         startActivity(intent);
     }
 }
